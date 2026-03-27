71373bec30a3eb5ba2db794ab5fdaf5d1a61591b
Alex <ralex8217@gmail.com>
2026-03-27 15:57:58 -0600

    ya me hartex2

--- api-crud/src/index.js
+++ api-crud/src/index.js
@@ -7,10 +7,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors()); 
+
 app.use(express.json());
 
-// RUTAS
 app.use("/api/plantas", plantasRoutes);
 
 const PORT = process.env.PORT || 3000;
